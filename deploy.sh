163aaee1ec330b15583ff2389a61dc9c2ac95e77
lzane 李澤帆 <cn.zanelee@gmail.com>
2018-11-04 21:12:48 +0800

    [*] update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
-rm -rf public
+
+if [ -d "for_deploy" ]; then
+    rm -rf for_deploy
+fi
+
+mkdir for_deploy
+cd for_deploy
+git clone https://github.com/lzane/blog
+cd blog
 hugo -t cocoa --baseURL="https://www.lzane.com"
 cp -r ./supplement/public/ public
-python3 ./deploy.py  
+python3 ./deploy.py
+cd ../..
+rm -rf for_deploy
